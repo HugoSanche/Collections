24f438a036a9fa3e5f97e764f09f57da281a5b63
HugoSanche <hbmclaren@gmail.com>
2023-04-16 19:57:35 -0600

    Modify class Adventure to convert in inmutable class

--- Adventure/src/Location.java
+++ Adventure/src/Location.java
@@ -4,18 +4,16 @@ public class Location {
     private final int idLocation;
     private final String description;
     private final Map<String, Integer> exits;
-    public Location(int idLocation, String description) {
+    public Location(int idLocation, String description,Map<String,Integer> exits) {
         this.idLocation = idLocation;
         this.description = description;
-        this.exits= new HashMap<>();
+        this.exits= new HashMap<String,Integer>(exits);
         this.exits.put("Q",0);
     }
-    public void addExist(String description, int idLocation){
-        exits.put(description,idLocation);
-    }
-    public int getIdLocation() {
-        return idLocation;
-    }
+//    public void addExist(String description, int idLocation){
+//        exits.put(description,idLocation);
+//    }
+
     public String getDescription() {
         return description;
     }
--- Adventure/src/Main.java
+++ Adventure/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Map;
 import java.util.Scanner;
 public class Main {
     enum Cardinal{NORTH,EAST,WEST,SOUTH }
-    public static HashMap<Integer,Location> locations=new HashMap<>();
+    final private static HashMap<Integer,Location> locations=new HashMap<>();
     public static void main(String[] args) {
         // Change the program to allow players to type full words, or phrases, then move to the
         // correct location based upon their input.
@@ -14,38 +14,38 @@ public class Main {
         // Single letter commands (N, W, S, E, Q) should still be available.
 
         Scanner scanner=new Scanner(System.in);
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
 
-        Map<String, Integer> tempExit=new HashMap<String,Integer>();
+        Map<String, Integer> tempExit=new HashMap<>();
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
         tempExit.put("W",2);
-
-        tempExit=new HashMap<String,Integer>();
         tempExit.put("E",3);
         tempExit.put("S",4);
         tempExit.put("N",5);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
 
         tempExit=new HashMap<String,Integer>();
         tempExit.put("N",5);
+        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
 
         tempExit=new HashMap<String,Integer>();
         tempExit.put("W",1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
+
 
         tempExit=new HashMap<String,Integer>();
         tempExit.put("N",1);
         tempExit.put("W",2);
+        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
 
         tempExit=new HashMap<String,Integer>();
         tempExit.put("S",1);
         tempExit.put("W",2);
+        locations.put(5, new Location(5, "You are in the forest",tempExit));
 
         int loc=1;
         while (true){
             System.out.println(locations.get(loc).getDescription());
+            tempExit.remove("S");
             if (loc==0){
                 break;
             }
