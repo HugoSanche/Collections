9e35b5808f3668ce30f162dc957cb03a6a1b88b2
HugoSanche <hbmclaren@gmail.com>
2023-06-21 18:48:18 -0600

    Work with single dimension array

---
+++ Collections_Overview/src/SingleDimensionArray.java
@@ -0,0 +1,42 @@
+public class SingleDimensionArray {
+    int arr[]=null;
+    public SingleDimensionArray(int sizeOfArray){
+        arr=new int[sizeOfArray];
+        for (int i=0; i<arr.length; i++){
+            arr[i]=Integer.MIN_VALUE;
+            System.out.println(arr[i]);
+        }
+    }
+    public void insert(int location, int valueToBeInserted){
+        try{
+            if (arr[location]==Integer.MIN_VALUE){
+                arr[location]=valueToBeInserted;
+                System.out.println("Value was inserted");
+            }
+            else{
+                System.out.println("This cell is already occupied");
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid index to access array"+ e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
