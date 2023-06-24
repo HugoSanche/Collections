ec03b931f1c65cab4957f7d393941cbb8944ea76
HugoSanche <hbmclaren@gmail.com>
2023-06-24 16:41:39 -0600

    Finish exercise LeetCode find Top Two Scores

--- LeetCode_Arrays/src/Main.java
+++ LeetCode_Arrays/src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[][] myArray2D= {{1,2,3},
                             {4,5,6},
                             {7,8,9}};
+        //excercise sum diagonal
         int sum=sumDiagonal.sumDiagonalElements(myArray2D);
         System.out.println(sum);
+
+        //exercise toptwoScores
+
+        int[] myArray = {84,85,86,87,85,90,85,83,23,45,84,1,2,0};
+        int[] top=FindTopTwoScores.findTopTwoScores(myArray);
+        System.out.println(Arrays.toString(top));
     }
 }
