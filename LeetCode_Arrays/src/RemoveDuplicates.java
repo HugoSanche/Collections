02ed0d3dcd9f5d3ed991ebe9e76fd2f22c7cba70
HugoSanche <hbmclaren@gmail.com>
2023-06-28 18:32:17 -0600

    remove duplicates

--- LeetCode_Arrays/src/RemoveDuplicates.java
+++ LeetCode_Arrays/src/RemoveDuplicates.java
@@ -2,24 +2,21 @@ import java.util.Arrays;
 
 public class RemoveDuplicates {
     public static int[] removeDuplicates(int[] arr) {
-        int[] newArray=new int [arr.length-1];
+        int n=arr.length;
+        int[] newArray=new int [n];
         int index=0;
-        //{1, 1, 2, 2, 3, 4, 5};
-        for (int i=1; i<arr.length-1; i++){
-            newArray[0]=arr[0];
-
-            System.out.println("i "+i+" "+arr[i]+" "+arr[i+1]);
-            if (arr[i]!=arr[i+1]){
-                System.out.println("YYYYYYYYYY");
-                System.out.println("X "+newArray[index]+" "+arr[i+1]);
-
-
-                newArray[index]=arr[i+1];
-                System.out.println(Arrays.toString(newArray));
-                System.out.println("YYYYYYYYYY");
-                index=index+1;
+        for (int i=0; i<n; i++){
+            boolean bandera=false;
+            for(int j=i+1; j<n; j++){
+                if (arr[i]==arr[j]){
+                    bandera=true;
+                    break;
+                }
+            }
+            if(!bandera){
+                newArray[index++]=arr[i];
             }
         }
-        return newArray;
+        return Arrays.copyOf(newArray,index);
     }
 }
