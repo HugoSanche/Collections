b03d14e7f803598ee8cfc260afe10b4d1f0cb94d
HugoSanche <hbmclaren@gmail.com>
2023-06-28 18:32:47 -0600

    remove duplicates sorted array

--- LeetCode_Arrays/src/RemoveDuplicatesSortedArray.java
+++ LeetCode_Arrays/src/RemoveDuplicatesSortedArray.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class RemoveDuplicatesSortedArray {
+public class RemoveDuplicatesSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        //TODO
+        int n=nums.length;
+        int cont=0;
+        for(int i=0;i<n-1;i++){
+            if (nums[i]==nums[i+1]){
+                cont++;
+            }
+        }
+        return n-cont;
+    }
+
 }
