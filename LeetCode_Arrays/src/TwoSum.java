78e330ae737847bc6488f1dab7e318b2ecb013d6
HugoSanche <hbmclaren@gmail.com>
2023-06-28 18:34:00 -0600

    add two sum

--- LeetCode_Arrays/src/TwoSum.java
+++ LeetCode_Arrays/src/TwoSum.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class TwoSum {
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        // TODO
+        for (int i=0; i<nums.length; i++){
+            for(int j=1; j<nums.length; j++){
+                if((nums[i]+nums[j])==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{0,0};
+    }
+
 }
