171a89a021f05b3973d01486c13d616b917cdf14
HugoSanche <hbmclaren@gmail.com>
2023-07-10 18:58:09 -0600

    add excercise Remove Element

--- ArrayList/src/RemoveElement.java
+++ ArrayList/src/RemoveElement.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class RemoveElement {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveElement {
+
+    //time Complexity: O(N)
+    //Space Complexity O(1)
+    public static void main(String[] args) {
+        ArrayList<String> myArray=new ArrayList<>(Arrays.asList("A","B","C","D","E","F"));
+        myArray.remove("D");
+        System.out.println(myArray);
+
+    }
 }
