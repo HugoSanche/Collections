d34b93fb7489883931de9ffa04b32bf4a907a31c
HugoSanche <hbmclaren@gmail.com>
2023-07-10 19:02:21 -0600

    add excercise  print unordened pairs

--- ArrayList/src/Main.java
+++ ArrayList/src/Main.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
+
+//add() – takes O(1) time; however, worst-case scenario, when a new array has to be created and all the elements copied to it, it's O(n)
+//add(index, element) – on average runs in O(n) time
+//get() – is always a constant time O(1) operation
+//remove() – runs in linear O(n) time. We have to iterate the entire array to find the element qualifying for removal.
+//indexOf() – also runs in linear time. It iterates through the internal array and checks each element one by one, so the time complexity for this operation always requires O(n) time.
+//contains() – implementation is based on indexOf(), so it'll also run in O(n) time.
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -10,16 +18,21 @@ public class Main {
         ArrayList myArrayList=new ArrayList();
 
         myArrayList.add("H");
-        myArrayList.add(1);
+        myArrayList.add(0,5);
 
         //Initialized by 10 elements
-        ArrayList test=new ArrayList(10);
+        ArrayList<Integer> test=new ArrayList<Integer>(10);
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        test.add(4);
+
+        //will mark exception IndexOutOfBoundsException
+        test.add(5,7);
+        System.out.println(test);
 
         //only integers
         ArrayList<Integer> arrayList=new ArrayList<>();
-        
-
-
 
         System.out.println(myArrayList);
 
--- LeetCode_ArraysList/src/PrintUnorderedPairs.java
+++ LeetCode_ArraysList/src/PrintUnorderedPairs.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class PrintUnorderedPairs {
+
+//time complexity O(N2)
+public class PrintUnorderedPairs {
+    public static void main(String[] args) {
+        int [] arrayNumbers={1,2,3,4,5};
+        PrintUnorderedPairs print=new PrintUnorderedPairs();
+        print.printPairs(arrayNumbers);
+    }
+    public void printPairs(int[] array){
+        for(int i=0;i<array.length; i++){ //..... O(N)
+            for(int j=i+1;j<array.length; j++ ){ //..... O(N)
+                System.out.print(array[i]+""+array[j]);// ..... O(i)
+            }
+            System.out.println();
+        }
+    }
 }
