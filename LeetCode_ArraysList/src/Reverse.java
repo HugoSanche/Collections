d5c9337ad53ed40c4f5d96a97274021dd9602405
HugoSanche <hbmclaren@gmail.com>
2023-07-11 23:08:51 -0600

    add exercise Reverse array

--- LeetCode_ArraysList/src/Reverse.java
+++ LeetCode_ArraysList/src/Reverse.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Reverse {
+import java.util.Arrays;
+
+public class Reverse {
+    public static void main(String[] args) {
+
+        Reverse reverse=new Reverse();
+        int [] arrayNumbers={4,1,5,9,7,5};
+        reverse.reverse(arrayNumbers);
+    }
+    public void reverse(int[] array){
+        System.out.println(array.length);
+        for (int i=0; i<array.length/2; i++){
+            int other=array.length-i-1; //save the last index
+            int temp=array[i]; //
+
+            array[i]=array[other];
+            array[other]=temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
 }
