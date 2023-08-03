8a750498cdd9afc2b4ae4fc0c21faff25d314aad
HugoSanche <hbmclaren@gmail.com>
2023-08-03 14:28:25 -0600

    Add method to create Doubly Linked List

--- DoublyLinkedList/src/DoubleLinkedList.java
+++ DoublyLinkedList/src/DoubleLinkedList.java
@@ -15,4 +15,38 @@ public class DoubleLinkedList {
         size=1;
         return head;
     }
+
+    //Time Complexity: O(N)
+    //Space Complexity: O(1)
+    public void insertDLL(int value, int location){
+        DoublyNode newNode =new DoublyNode();
+        newNode.value=value;
+        if (head==null){
+            createNode(1);
+        }
+        else if (location==0){
+            newNode.next=head;
+            newNode.previous=null;
+            head.previous=newNode;
+            head=newNode;
+        }else if (location>=size){
+            newNode.next=null;
+            newNode.previous=tail;
+            tail.next=newNode;
+            tail=newNode;
+        }else{
+            DoublyNode tempNode= new DoublyNode();
+            int index=0;
+            while (index<location-1){
+                tempNode=tempNode.next;
+                index++;
+            }
+            newNode.next=tempNode.next;
+            newNode.previous=tempNode;
+            tempNode.next=newNode;
+            tempNode.next.previous=tempNode;
+            //newNode.next.previous=newNode;
+        }
+        size++;
+    }
 }
