a6b8b01dd67b047f5d5b754d51f247d32fd7470e
HugoSanche <hbmclaren@gmail.com>
2023-08-15 23:32:58 -0600

    implement cirular linked list

---
+++ Queue/Queue/src/QueueLinkedList.java
@@ -0,0 +1,7 @@
+public class QueueLinkedList {
+    LinkedList list;
+    public QueueLinkedList(){
+        list=new LinkedList();
+        System.out.println("The circular linked list successfully created");
+    }
+}
