bdb3868fee453f27ac3f8ea680d5203f1694359f
HugoSanche <hbmclaren@gmail.com>
2023-08-17 18:46:02 -0600

    Create a Stack and method isEmpty

---
+++ Stack/Stack/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Stack stack=new Stack(5);
+        boolean result=stack.isEmpty();
+        System.out.println(result);
+    }
+}
---
+++ Stack/Stack/src/Stack.java
@@ -0,0 +1,24 @@
+public class Stack {
+    int arr[];
+    int topOfStack;
+
+
+    //time complexity O(1)
+    //space complexity O(N)
+    public Stack(int size) {
+        this.arr = new int[size];
+        this.topOfStack = -1;
+        System.out.println("The Stack was created with size " + size);
+    }
+
+    //time complexity O(1)
+    //space complexity O(1)
+    public boolean isEmpty() {
+        if (topOfStack == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
