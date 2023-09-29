e382d4961cc91e764500ed623d1a135e6af00a6d
HugoSanche <hbmclaren@gmail.com>
2023-09-28 18:22:47 -0600

    add leetcode remove list elements

--- LeetCode_LinkedList/Exercises_LinkedList/src/RemoveListElements.java
+++ LeetCode_LinkedList/Exercises_LinkedList/src/RemoveListElements.java
@@ -1,29 +1,30 @@
 public class RemoveListElements {
     public ListNode removeElements(ListNode head, int val) {
-       ListNode tempNode=head, prev=head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
 
-
-        while (head.val==val && head!=null){
-           // prev=head.next;
-            head.next=null;
-            head=prev.next;
-            prev=prev.next;
-        }
-
-        head=head.next;
-        while(head!=null) {
-            if (head.val == val) {
-                prev.next = head.next;
-                head.next = null;
-                head = prev.next;
+        while(cur.next != null) {
+            if(cur.next.val == val) {
+                cur.next = cur.next.next;
             }
-            if (head!=null) {
-                if (head.val != val) {
-                    head = head.next;
-                    prev = prev.next;
-                }
+            else
+                cur = cur.next;
+        }
+        return dummy.next;
+    }
+    public ListNode removeElements2(ListNode head, int val) {
+        ListNode fakeHead=new ListNode(-1);
+        fakeHead.next=head;
+        ListNode currentNode=fakeHead;
+        while(currentNode.next!=null){
+            if (currentNode.next.val==val){
+                    currentNode.next=currentNode.next.next;
+            }
+            else{
+                currentNode=currentNode.next;
             }
         }
-        return tempNode;
+        return fakeHead.next;
     }
 }
